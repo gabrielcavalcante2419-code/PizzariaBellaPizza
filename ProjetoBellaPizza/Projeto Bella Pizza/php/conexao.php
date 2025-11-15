18401c23eb193df948cb586dfa028bf859816e14
gabrielcavalcante2419-code <gabriel.cavalcante2419@gmail.com>
2025-11-15 01:14:57 -0300

    Update conexao.php

--- ProjetoBellaPizza/Projeto Bella Pizza/php/conexao.php
+++ ProjetoBellaPizza/Projeto Bella Pizza/php/conexao.php
@@ -3,7 +3,7 @@ $host = 'bancodedadospizzaria.c5c8ea6m08um.us-east-1.rds.amazonaws.com';
 $usuario = 'admin';                  
 $senha = 'mnbvcxzasdfghjkl';                        
 $banco = 'modelo_bancodedados';
-$port = '3306'
+$port = '3306';
 
 
 // Criando a conexão
@@ -14,5 +14,6 @@ if ($conn->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 ?>
+
 
 
